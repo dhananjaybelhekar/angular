c0d8a4189467f88dc8be3287a2d726e7a32446dc
dhananjay belhekar <dbel431@gmail.com>
2017-09-22 17:41:16 +0530

    add blockchain

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 var SHA256 = require("crypto-js/sha256");
-class main
+class block
 {
-	show(data){
-		return SHA256(JSON.stringify(data)).toString();
+	constructor(index,data,phash = "")
+	{
+		this.index = index;
+		this.data = data;
+		this.phash = phash;
+		this.hash=this.show();
+
+
+	}
+	show(){
+		return SHA256(this.index+this.data).toString();
 
 	}
 }
-let a= new main();
-let b= a.show({phash:null,msg:"hii"});
-console.log(b);
+class Blockchanin
+{
+	constructor()
+	{
+		this.chain = [new block(0,"hi")];
+	}
+	createNew(data)
+	{
+		data.phash = this.getLetest().hash;
+		data.hash = data.show();
+		this.chain.push(data);
+	}
+	getLetest()
+	{
+		return this.chain[ this.chain.length - 1 ];
+	}
+}
 
+//let b= a.show({phash:null,msg:"hii"});
+//console.log(a);
+var blockchanin = new Blockchanin();
+//blockchanin.createNew(new );
+//blockchanin.createNew();
+blockchanin.createNew(new block(1,"dhananjay"));
+blockchanin.createNew(new block(3,"anjali"));
+console.log(blockchanin);
