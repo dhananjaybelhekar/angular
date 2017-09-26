1f1aba43f61247f654d80a09851d8226954b42f0
dhananjay431 <dbel431@gmail.com>
2017-09-26 15:18:59 +0530

    image upload

---
+++ plunk-liUfuaIgBuOhVKuT1Fs4/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const fs= require('fs');
+var bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.json({ limit: '50mb' }));
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/fileupload', function (req, res) {
+var d=new Date().valueOf();
+ var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+		
+    
+if("jpeg"==req.body.img.split(";")[0].split("/")[1])
+{
+var imageName = d + '.' + text + '.jpg';
+}
+if("gif"==req.body.img.split(";")[0].split("/")[1])
+{
+var imageName = d + '.' + text + '.gif';
+}
+if("x-icon"==req.body.img.split(";")[0].split("/")[1])
+{
+var imageName = d + '.' + text + '.ico';
+}
+if("png"==req.body.img.split(";")[0].split("/")[1])
+{
+var imageName = d + '.' + text + '.png';
+}
+
+
+
+
+var data = req.body.img.replace(/^data:image\/\w+;base64,/, "");
+
+var buf = new Buffer(data, 'base64');
+fs.writeFile(imageName, buf);
+res.send(imageName);
+
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
---
+++ plunk-liUfuaIgBuOhVKuT1Fs4/todo.js
@@ -0,0 +1,23 @@
+angular.module('todoApp', [])
+.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+}])
+  .controller('TodoListController', function() {
+    var todoList = this;
+  
+  });
