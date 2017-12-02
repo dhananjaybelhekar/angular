ed8fabe98f9b0648119987d99f341b9f6e2d08b1
dhananjay belhekar <dbel431@gmail.com>
2017-12-02 20:50:22 +0530

    all return cb function

--- javascript_code/cb.cb.js
+++ javascript_code/cb.cb.js
@@ -1,21 +1,27 @@
-function show1(cb){
-   console.log("show1 end");
-   cb();
+function show1(data,cb){
+   //console.log("show1 end");
+   return cb(data);
 }
-function show2(cb){
-   console.log("show2 end");
-   cb();
+function show2(data,cb){
+   //console.log("show2 end");
+   return  cb(data);
 }
-function show3(cb){
-   console.log("show3 end");
-   cb();
+function show3(data,cb){
+   data.show1.show2.show3 = 301;
+   return data;
+   //console.log("show3 end",data);
+  // return cb(data);
 }
-function main(cb){
-
-    show1(function(){
-        show2(function(){
-            show3(cb);
+function main(data,cb){
+    return show1(data,function(data){
+        data.show1 = {};
+       return  show2(data,function(data){
+        data.show1.show2 = {};
+           return  show3(data,cb);
         })
     })
 }
-main(function(){ console.log("all DONE")})
+var arr=[];
+for(var i=0;i<1000;i++)
+arr.push(main({number:i},function(){ console.log("all DONE")})) 
+console.log(arr);
