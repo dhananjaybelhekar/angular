454cf1d6f43bb14e6c1dcb721df20e15082b8609
dhananjay431 <dbel431@gmail.com>
2017-12-06 20:12:52 +0530

    nested

--- async/app2.js
+++ async/app2.js
@@ -3,14 +3,21 @@
 var mongoose = require("mongoose"),
 Schema = mongoose.Schema;
 mongoose.connect("mongodb://192.168.10.178/OCD_XML", { useMongoClient: true });
-
+var fs = require('fs');
 var orgScgema = new mongoose.Schema({
 	tag:Schema.Types.String,
     children:[Schema.Types.Mixed],
 });
 
 var _DB={},
-	_QR={qr1:{"_id" : new mongoose.Types.ObjectId("57640ac1c19caec92e9c30c1")},
+	_QR={qr:{"directoryId" : new mongoose.Types.ObjectId("57189b3e24d8bc65f4123bbf")},
+
+
+		qr1:{"_id" :{$in:[
+			new mongoose.Types.ObjectId("57640ac1c19caec92e9c30c1"),
+			// new mongoose.Types.ObjectId("57640ac1c19caec92e9c30c7"),
+			// new mongoose.Types.ObjectId("57640ac1c19caec92e9c30c8")
+			]} },
 	qr2:{"_id" : new mongoose.Types.ObjectId("57640ac1c19caec92e9c30c2")},
 	qr3:{"_id" : new mongoose.Types.ObjectId("57640ac1c19caec92e9c30c3")},
 };
@@ -26,38 +33,78 @@ _DB.txn_temp= mongoose.model('TXN_Temp',new mongoose.Schema({
 
 function fun1(cb,d){
 	_DB.txn_organization.find(_QR.qr1,{name:1}).exec(function(err,data){
-		console.log("fun1");
-		// d.tag="sec";
-		// d.children=[];
-		d={tag:"fun1","children":data};
-		cb(d);
+		data.map((beach)=>{
+			d.push(
+				{
+					tag:"fun1",
+					children:[beach],
+					id:1,
+					parent:0
+				});
+			cb(d);
+		});
 	})
 }
 function fun2(cb,d){
 	_DB.txn_organization.find(_QR.qr2,{name:1}).exec(function(err,data){
-		
-		d.children[0].children={tag:"fun2","children":data};
-		//d.children.children[1]={tag:"fun2","children":data};
+		d.push({
+			tag:"fun2",
+			children:data,
+			id:2,
+			parent:1
+	});
 		cb(d);
 	})
 }
 function fun3(cb,d){
 	_DB.txn_organization.find(_QR.qr3,{name:1}).exec(function(err,data){
-		d.children[0].children[0].children={tag:"fun3","children":data};
+		d.push({
+			tag:"fun2",
+			"children":data,
+			id:3,
+			parent:2,
+			value:"sdf"
+		});
 		cb(d);
 	})
 }
 
 function run(cb){
 	fun1((data)=>{
-		cb(data);
 		 fun2((data)=>{
-		//	cb(data);
 		 	fun3(cb,data)
 		 },data)
-	},{})
+	},[])
 }
 run(function(data){
-	console.log("END ALL",JSON.stringify(data,null,5));
+	console.log("END ALL",
+		savefile(JSON.stringify(bunflatten(data),null,5)).then((res)=>{
+			console.log("done FILE");
+		})
+		);
 })
 
+function savefile(tag){
+	return new Promise(function(resolve,reject){
+		fs.appendFile('xml.txt',tag, function(err) {
+        if (err) throw reject(err);
+      });
+		resolve(true)
+	});
+}
+
+function bunflatten(nodes) {
+    var map = {}, _NODE, roots = [];
+    for (var i = 0; i < nodes.length; i += 1) {
+        _NODE = nodes[i];
+        _NODE.children = [];
+        map[_NODE.id] = i;
+        if (_NODE.parent !== 0) {
+            _NODE.children = [];
+            nodes[map[_NODE.parent]].children.push(_NODE);
+        } else {
+            roots.push(_NODE);
+        }
+    }
+    return roots;
+}
