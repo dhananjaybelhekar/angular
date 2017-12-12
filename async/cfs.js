d764059d60c35c58b0bdc4fac6744bdaa5ae4c28
dhananjay belhekar <dbel431@gmail.com>
2017-12-12 21:20:18 +0530

    home changes

--- async/cfs.js
+++ async/cfs.js
@@ -6,8 +6,8 @@ ProgressBar = require('progress'),
 jsonata = require("jsonata"),
 cleanDeep = require('clean-deep'),
 Schema = mongoose.Schema;
-//mongoose.connect("mongodb://192.168.10.178/OCD_XML", { useMongoClient: true });
-mongoose.connect("mongodb://localhost/tw-UAT-20161212", { useMongoClient: true });
+mongoose.connect("mongodb://192.168.10.178/OCD_XML", { useMongoClient: true });
+//mongoose.connect("mongodb://localhost/tw-UAT-20161212", { useMongoClient: true });
 var fs = require('fs');
 var orgScgema = new mongoose.Schema({
 	// personnel:Schema.Types.Mixed,
@@ -39,6 +39,42 @@ var _DB={},
 			}
 		},
 	],
+	CFSINDEX:    [
+    // Stage 1
+    {
+      $match: {
+      "listingName":{$ne:null}
+      }
+    },
+
+    // Stage 2
+    {
+      $group: { 
+          "_id" : {
+              "listingName" : "$listingName", 
+              "specificationType" : "$specificationType"
+          }, 
+          "Org" : {
+              "$addToSet" : "$organizationId"
+          }
+      }
+    },
+
+    // Stage 3
+    {
+      $group: {
+      _id:"$_id.listingName",
+      data:{$push:{specificationType:"$_id.specificationType","Org":"$Org"}}
+      }
+    },
+
+    // Stage 4
+    {
+      $sort: {
+      "_id":1
+      }
+    }
+  ]
 };
 
 _DB.txn_personnel= mongoose.model('TXN_Personnel',new mongoose.Schema({
@@ -51,6 +87,12 @@ _DB.txn_personnel= mongoose.model('TXN_Personnel',new mongoose.Schema({
     }
 
 }));
+
+_DB.txn_chapterspecifications= mongoose.model('txn_chapterspecification',new mongoose.Schema({
+
+}));
+
+
 _DB.txn_organization = mongoose.model('TXN_Organization',orgScgema);
 
 _DB.txn_temp= mongoose.model('TXN_Temp',new mongoose.Schema({
@@ -59,60 +101,89 @@ _DB.txn_temp= mongoose.model('TXN_Temp',new mongoose.Schema({
 
 function fun1(cb,d){
     
-	_DB.txn_organization.aggregate(evaluate(_QR.cfs)).exec(function(err,data){
+	_DB.txn_chapterspecifications.aggregate(evaluate(_QR.CFSINDEX)).exec(function(err,data){
 		 _DB.txn_organization.populate(data,
 		 	{
-		 		path: 'personnel', 
+		 		path: 'data.Org', 
 		 		//sort:{"name.first": 1},
+		 		select:{name:1,org_id:1},
 		 		match: { deleted:false }
 		 	},(err1,data2)=>{
 
-    fs.appendFile( 'per.txt',JSON.stringify(data2[2],null,2), function(err) {
-        if (err) throw reject(err);
-      });
+
 
 				var xxx=[];
-				xxx.push({name:"Sec",attrs:{mood:'sad'},id:1,parent:0 }); 
-				data2.map((zzz)=>{
-				zzz = JSON.parse(JSON.stringify(zzz));
-				xxx.push({name:"Org",id:2,parent:1 });
-				xxx.push({name:"OrgInfo",id:7,parent:2});
-			//	xxx.push({name:"ID",text:zzz._id,id:13,parent:7});
-                xxx.push({name:"OrgId", text:zzz.org_id,id:9,parent:7});
-				xxx.push({name:"OrgName",text:zzz.name,id:3,parent:7});
 				
-				xxx.push({name:"listingTypeInfo",id:4,parent:2});
-				// var res = jsonata("listingType[listingName='Foreign Banks']").evaluate(zzz); 
-				// console.log(res)
+				xxx.push({name:"component",attrs:{type:'CFS-Regular'},id:1,parent:0 }); 
+				xxx.push({name:"Sec",id:2,parent:1 }); 
+				
+				data2.map((zzz)=>{
+					xxx.push({name:"Chapter",id:3,parent:2}); 	
+					xxx.push({name:"ChapterName",id:4,parent:3,text:zzz._id }); 	
+					
+					for(var org in zzz.data)
+					{
+						var temp = JSON.parse(JSON.stringify(zzz.data[org]));
+						xxx.push({name:"SpecificationType",id:5,parent:3}); 	
+						xxx.push({name:"SpecificationTypeName",id:6,parent:5,text:temp.specificationType }); 	
+    // fs.appendFile( 'per.txt',JSON.stringify(temp,null,2), function(err) {
+    //     if (err) throw reject(err);
+    //   });
 
-				for(var dd in zzz.listingType)
-				{
-					xxx.push({name:"listingType", parent:4, id:5, }); 
-					xxx.push({name:"featuresName", parent:5, id:6, text:zzz.listingType[dd].listingName }); 
-				}
-				xxx.push({name:"keyPersonnelInfo",id:7,parent:2});
-				//console.log(jsonata("name").evaluate(_.cloneDeep(zzz.personnel))); 
-				//console.log(_.cloneDeep(zzz.personnel)); 				
-				for(var dd in zzz.personnel)
-				{
-				// console.log(zzz.personnel[dd]);
-					xxx.push({name:"PersonnelInfo",  id:8, parent:7,}); 
-					xxx.push({name:"employee_id",  id:12, parent:8, text:zzz.personnel[dd].employee_id}); 
-					xxx.push({name:"PersonneltitleMasterName",  id:11, parent:8, text:zzz.personnel[dd].titleMasterName}); 
-					xxx.push({name:"PersonnelFirstName",  id:9, parent:8, text:zzz.personnel[dd].name.first }); 
-					xxx.push({name:"PersonnelLastName",  id:10, parent:8, text:zzz.personnel[dd].name.last }); 
-				}
+
+						 xxx.push({name:"Org",id:7,parent:3}); 	
+						// 
+						 for(var orgOfOrg in temp.Org)
+						{
+							
+							xxx.push({name:"OrganizationID",id:8,parent:7,text:temp.Org[orgOfOrg].org_id }); 	
+							xxx.push({name:"OrganizationName",id:9,parent:7,text:temp.Org[orgOfOrg].name}); 	
+						}
+						
+					}
 
 				});
+				
+
+				
+			// 	data2.map((zzz)=>{
+			// 	zzz = JSON.parse(JSON.stringify(zzz));
+			// 	xxx.push({name:"Org",id:2,parent:1 });
+			// 	xxx.push({name:"OrgInfo",id:7,parent:2});
+			// //	xxx.push({name:"ID",text:zzz._id,id:13,parent:7});
+   //              xxx.push({name:"OrgId", text:zzz.org_id,id:9,parent:7});
+			// 	xxx.push({name:"OrgName",text:zzz.name,id:3,parent:7});
+				
+			// 	xxx.push({name:"listingTypeInfo",id:4,parent:2});
+			// 	// var res = jsonata("listingType[listingName='Foreign Banks']").evaluate(zzz); 
+			// 	// console.log(res)
+
+			// 	for(var dd in zzz.listingType)
+			// 	{
+			// 		xxx.push({name:"listingType", parent:4, id:5, }); 
+			// 		xxx.push({name:"featuresName", parent:5, id:6, text:zzz.listingType[dd].listingName }); 
+			// 	}
+			// 	xxx.push({name:"keyPersonnelInfo",id:7,parent:2});
+			// 	//console.log(jsonata("name").evaluate(_.cloneDeep(zzz.personnel))); 
+			// 	//console.log(_.cloneDeep(zzz.personnel)); 				
+			// 	for(var dd in zzz.personnel)
+			// 	{
+			// 	// console.log(zzz.personnel[dd]);
+			// 		xxx.push({name:"PersonnelInfo",  id:8, parent:7,}); 
+			// 		xxx.push({name:"employee_id",  id:12, parent:8, text:zzz.personnel[dd].employee_id}); 
+			// 		xxx.push({name:"PersonneltitleMasterName",  id:11, parent:8, text:zzz.personnel[dd].titleMasterName}); 
+			// 		xxx.push({name:"PersonnelFirstName",  id:9, parent:8, text:zzz.personnel[dd].name.first }); 
+			// 		xxx.push({name:"PersonnelLastName",  id:10, parent:8, text:zzz.personnel[dd].name.last }); 
+			// 	}
+
+			// 	});
 				cb(xxx); 	
 		 });
 		
 	})
 }
 function run(cb){
-	console.log("Start....");
 	fun1((data)=>{
-		console.log("Fun1....");
 		cb(data);
 	},[])
 }
